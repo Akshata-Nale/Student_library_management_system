c6fca6c77fe07bb27172f4392f7263a7677daafa
Akshata Nale <akshatanale111@gmail.com>
2023-04-02 22:26:50 +0530

    Code for transactions

--- src/main/java/com/example/Student_Library_Management_System/Repositories/TransactionRepository.java
+++ src/main/java/com/example/Student_Library_Management_System/Repositories/TransactionRepository.java
@@ -2,8 +2,15 @@ package com.example.Student_Library_Management_System.Repositories;
 
 import com.example.Student_Library_Management_System.Models.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions,Integer> {
+
+    @Query(value = "select * from transactions t where t.book_id=:bookId and t.card_id=:cardId and is_issue_operation=true",nativeQuery = true)
+    List<Transactions> getTransactionsForBookAndCard(int bookId, int cardId);
+    //whenever you want to refer to the parameters you use ":" and variable name should be same
 }
--- src/main/java/com/example/Student_Library_Management_System/Services/TransactionService.java
+++ src/main/java/com/example/Student_Library_Management_System/Services/TransactionService.java
@@ -1,10 +1,20 @@
 package com.example.Student_Library_Management_System.Services;
 
 import com.example.Student_Library_Management_System.DTO.IssueBookRequestDto;
+import com.example.Student_Library_Management_System.Enums.CardStatus;
+import com.example.Student_Library_Management_System.Enums.TransactionStatus;
+import com.example.Student_Library_Management_System.Models.Book;
+import com.example.Student_Library_Management_System.Models.Card;
+import com.example.Student_Library_Management_System.Models.Transactions;
+import com.example.Student_Library_Management_System.Repositories.BookRepository;
+import com.example.Student_Library_Management_System.Repositories.CardRepository;
 import com.example.Student_Library_Management_System.Repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TransactionService {
@@ -12,9 +22,87 @@ public class TransactionService {
     @Autowired
     TransactionRepository transactionRepository;
 
-    public String issueBook(@RequestBody IssueBookRequestDto issueBookRequestDto){
-        return "";
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    CardRepository cardRepository;
+
+    public String issueBook(@RequestBody IssueBookRequestDto issueBookRequestDto) throws Exception{
+
+        int bookId = issueBookRequestDto.getBookId();
+        int cardId = issueBookRequestDto.getCardId();
+
+        //Get the book entity and card entity to set the transactions attributes(2 rules)
+
+        Book book = bookRepository.findById(bookId).get();
+
+        Card card = cardRepository.findById(cardId).get();
+
+        //Final aim : make a transaction Entity -> Set its attributes -> Save
+
+        Transactions transactions = new Transactions();
+
+        //Setting attributes
+        transactions.setBook(book);
+        transactions.setCard(card);
+        transactions.setTransactionId(UUID.randomUUID().toString());
+        transactions.setIssueOperation(true);
+
+        transactions.setTransactionStatus(TransactionStatus.PENDING);
+        //Attribute left : Success/Failure
+        //Checking validations
+        if(book==null || book.isIssued()==true){
+            transactions.setTransactionStatus(TransactionStatus.FAILED);
+            transactionRepository.save(transactions);
+            throw new Exception("Book is not available");
+
+        }
+
+        if(card==null || (card.getCardStatus() != CardStatus.ACTIVATED)){
+            transactions.setTransactionStatus(TransactionStatus.FAILED);
+            transactionRepository.save(transactions);
+            throw new Exception("Card is not valid");
+        }
+
+        //We have reached a success case now
+        transactions.setTransactionStatus(TransactionStatus.SUCCESS);
+
+        //Set attributes of BOOK
+        book.setIssued(true);
+        //Between BOOK and TRANSACTIONS : Bidirectional
+        List<Transactions> listOfTransactionForBook = book.getTransactionsList();
+        listOfTransactionForBook.add(transactions);
+        book.setTransactionsList(listOfTransactionForBook);
+
+        //Changes in the card
+        //BOOK and CARD
+        List<Book> issuedBooksForCard = card.getBooksIssued();
+        issuedBooksForCard.add(book);
+        card.setBooksIssued(issuedBooksForCard);
+
+        //CARD and TRANSACTION : Bidirectional(in the parent class)
+        List<Transactions> transactionsListForCard = card.getTransactionsList();
+        transactionsListForCard.add(transactions);
+        card.setTransactionsList(transactionsListForCard);
+
+
+        //save the parent object
+        cardRepository.save(card);
+        //automatically by cascading book and transaction will be saved
+
+        return "Book issued successfully";
     }
+
+    public String getTransaction(int bookId, int cardId){
+        List<Transactions> transactionsList = transactionRepository.getTransactionsForBookAndCard(bookId,cardId);
+
+        String transactionId = transactionsList.get(0).getTransactionId();
+
+        return transactionId;
+
+    }
+
 
 
 }
